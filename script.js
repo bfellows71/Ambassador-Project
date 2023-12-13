70bf7c7ec6b9d49ec1954d6199d83a422249651b
FAST <fello005@rangers.uwp.edu>
2023-12-12 22:47:51 -0800

    new vers

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ document.addEventListener('DOMContentLoaded', function () {
         body.classList.toggle('menu-open');
     });
 });
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Show/hide scroll-to-top button based on scroll position
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var scrollToTopButton = document.getElementById("scroll-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
